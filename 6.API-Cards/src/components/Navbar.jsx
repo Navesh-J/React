30c13ba8734c6d201f066fc1a34d9d040bbeed51
Navesh <navesh.professional@gmail.com>
2024-08-01 12:45:14 +0530

    Generate cards using APIs

---
+++ 6.API-Cards/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+   <nav>
+    <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contacts Us</li>
+    </ul>
+   </nav>
+  )
+}
+
+export default Navbar
