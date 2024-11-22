6e344aa8b3ef9174fb9a174d5ccd8a79eca0d9ed
Navesh <navesh.professional@gmail.com>
2024-11-22 17:20:29 +0530

    A basic counter project

---
+++ 01_counter/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+//This counter ranges from 0 to 20
+function App() {
+  const [count, setCount] = useState(0)
+  const increment=()=>{
+    if(count<20)
+    setCount(count+1)
+  }
+  const decrement=()=>{
+    if(count>0)
+    setCount(count-1)
+  }
+  return (
+    <>
+    <h1>The currrent count is: {count}</h1>
+    <div><button onClick={increment}>Increase Counter</button></div>
+    <div><button onClick={decrement}>Decrease Counter</button></div>
+    </>
+  )
+}
+
+export default App
